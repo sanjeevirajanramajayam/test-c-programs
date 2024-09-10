53c9b0bcbc272a8425de1513b044a95a35ddca9a
mainman24 <163754159+mainman24@users.noreply.github.com>
2024-09-10 14:25:02 +0530

    Update twostrlenimplementations.c

--- twostrlenimplementations.c
+++ twostrlenimplementations.c
@@ -7,6 +7,8 @@ int main()
     ps1 = &s1[0]; // or ps1 = s1;
     int x = strlen1(ps1);
     printf("%d", x);
+    int x = strlen2(ps1);
+    printf("%d", x);
 }
 
 int strlen1(s) // s is a pointer variable. s = &str
@@ -24,3 +26,13 @@ char *s; //
 
 
 
+int strlen2(s)
+char *s
+{
+    char *p = s // define a pointer of char type equal to pointer s
+
+    while (*p != '\0')
+        p++;
+
+    return (p-q); // Subtraction of two pointers give difference of position in array
+}
